8c7bdc297e50f07e9f4967ac3f15096656c0dda3
michaengeser <125463198+michaengeser@users.noreply.github.com>
2024-11-15 15:28:24 +0100

    adapt motion parameter plotting

--- code/utilities/plot_motion_params.py
+++ code/utilities/plot_motion_params.py
@@ -1,42 +1,53 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # define subject, path, and runs
 sub = int(input('Enter the subject number: '))
-sub_id = f'sub-{sub:03d}xxxx'
-path = (
-    '/Users/philippflieger/PhD/projects/fusion-study_scene-attractiveness/'
-    f'sourcedata/fmri/bids/{sub_id}/func'
+sub_id = f'sub-{sub:03d}'
+sub_idx = f'sub-{sub:03d}xxxx'
+path = os.path.join(
+    os.path.abspath(os.path.join(os.getcwd(), '..', '..')),  
+    'derivatives', sub_id, 'func'
 )
-runs = 8  # 1 localizer, 7 experimental runs
+runs = 12  # 1 localizer, 10 experimental runs, 1 resting state
 
 # load each file and plot motion parameters (translation and rotation in X/Y/Z)
 for run in range(runs):
-    if run+1 == 1:  # localizer
-        filename = f'rp_{sub_id}_task-localizer_bold_00001.txt'
+    if run == 0:  # localizer
+        filename = f'rp_{sub_idx}_task-localizer_bold_00001.txt'
+    elif run == runs - 1:  # resting state 
+        filename = f'rp_{sub_idx}_task-rest_bold_00001.txt'
     else:  # experimental runs
-        filename = f'rp_{sub_id}_task-scenes_run-{run}_bold_00001.txt'
+        filename = f'rp_{sub_idx}_task-scenes_run-{run}_bold_00001.txt'
 
-    # open the file as space-separated values
-    motion_params = pd.read_csv(
-        f'{path}/{filename}', sep=r'\s+', header=None
-    )
+    # Full file path
+    file_path = os.path.join(path, filename)
 
-    # plot translation (columns 1:3) and rotation (columns 4:6)
-    col_ranges = [(0, 3), (3, 6)]
-    titles = ['Translation', 'Rotation']
+    # Load the file if it exists
+    if os.path.exists(file_path):
+        # open the file as space-separated values
+        motion_params = pd.read_csv(file_path, sep=r'\s+', header=None)
 
-    for subplot, col_range, title in zip(range(2), col_ranges, titles):
-        plt.subplot(2, 1, subplot+1)
-        plt.plot(motion_params.iloc[:, col_range[0]:col_range[1]])
-        plt.title(title)
-        plt.legend(['X', 'Y', 'Z'])
-        plt.subplots_adjust(hspace=0.5)  # add more vertical space between subplots
+        # plot translation (columns 1:3) and rotation (columns 4:6)
+        col_ranges = [(0, 3), (3, 6)]
+        titles = ['Translation', 'Rotation']
 
-    # add title
-    if run+1 == 1:  # localizer
-        plt.suptitle(f'Subject {sub}, Localizer Run: Translation and Rotation')
-    else:  # experimental runs
-        plt.suptitle(f'Subject {sub}, Run {run}: Translation and Rotation')
+        for subplot, col_range, title in zip(range(2), col_ranges, titles):
+            plt.subplot(2, 1, subplot+1)
+            plt.plot(motion_params.iloc[:, col_range[0]:col_range[1]])
+            plt.title(title)
+            plt.legend(['X', 'Y', 'Z'])
+            plt.subplots_adjust(hspace=0.5)  # add more vertical space between subplots
 
-    plt.show()
+        # add title
+        if run == 0:  # localizer
+            plt.suptitle(f'Subject {sub}, Localizer Run: Translation and Rotation')
+        elif run == runs - 1:  # resting state
+            plt.suptitle(f'Subject {sub}, Resting State Run: Translation and Rotation')
+        else:  # experimental runs
+            plt.suptitle(f'Subject {sub}, Run {run}: Translation and Rotation')
+
+        plt.show()
+    else:
+        print(f"File not found: {file_path}")
